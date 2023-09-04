22de3d16a946d76a90917c422df8248a458a75d2
Luke Young <bored-engineer@users.noreply.github.com>
2023-09-04 16:17:13 -0700

    Update unsupported.go

--- unsupported.go
+++ unsupported.go
@@ -5,6 +5,9 @@ package launchd
 
 import "syscall"
 
+// Supported is true if launchd socket activation is supported on this platform
+const Supported = false
+
 // Always returns syscall.ENOSYS on unsupported platforms
 func libxpc_launch_activate_socket(name string) ([]int, error) {
 	return nil, syscall.ENOSYS
